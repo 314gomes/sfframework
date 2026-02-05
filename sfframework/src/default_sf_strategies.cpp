dba804c0e4d68e39b9e51b27c53e935bbf32ae61
João Pedro Gomes <joaop.gomes@usp.br>
2026-02-05 11:27:46 -0300

    Adds WeightedSummation scalar field strategy

--- sfframework/src/default_sf_strategies.cpp
+++ sfframework/src/default_sf_strategies.cpp
@@ -109,7 +109,55 @@ namespace sfframework
     }
   };
 
+  class WeightedSummation : public SFStrategy
+  {
+    void onInitialize(grid_map::GridMap &grid_map) override
+    {
+      node_->declare_parameter(name_ + ".input_layers", std::vector<std::string>());
+      node_->declare_parameter(name_ + ".weights", std::vector<double>());
+      node_->declare_parameter(name_ + ".output_layer", "potential");
+
+      grid_map.add(node_->get_parameter(name_ + ".output_layer").as_string(), 0.0);
+    }
+
+    void onProcess(PartitioningContext & /*context*/, grid_map::GridMap &grid_map) override
+    {
+      auto input_layers = this->node_->get_parameter(name_ + ".input_layers").as_string_array();
+      auto weights = this->node_->get_parameter(name_ + ".weights").as_double_array();
+      auto output_layer = this->node_->get_parameter(name_ + ".output_layer").as_string();
+
+      if (input_layers.size() != weights.size())
+      {
+        RCLCPP_ERROR(node_->get_logger(), "SF strategy plugin [%s] has a different number of input layers and weights. Skipping...", name_.c_str());
+        return;
+      }
+
+      // Check if every input layer exists
+      for (size_t i = 0; i < input_layers.size(); ++i){
+        if (!grid_map.exists(input_layers[i]))
+        {
+          RCLCPP_WARN(node_->get_logger(), "SF strategy plugin [%s] tried to find an input layer that does not exist. Skipping...", name_.c_str());
+          return;
+        }
+      }
+      
+      // Compute weighted summation
+      // #pragma omp parallel for
+      for (grid_map::GridMapIterator iterator(grid_map); !iterator.isPastEnd(); ++iterator)
+      {
+        double potential = 0.0;
+        for (size_t i = 0; i < input_layers.size(); ++i)
+        {
+          potential += weights[i] * grid_map.at(input_layers[i], *iterator);
+        }
+        grid_map.at(output_layer, *iterator) = potential;
+      }
+
+    }
+  };
+
 } // namespace sfframework
 
 PLUGINLIB_EXPORT_CLASS(sfframework::GaussianRepulsion, sfframework::SFStrategy)
 PLUGINLIB_EXPORT_CLASS(sfframework::ParabolicRest, sfframework::SFStrategy)
+PLUGINLIB_EXPORT_CLASS(sfframework::WeightedSummation, sfframework::SFStrategy)
